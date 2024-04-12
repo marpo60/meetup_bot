b98f15f71035321bec819c2608dd97a6e7f59c06
Marcelo Dominguez <952074+marpo60@users.noreply.github.com>
2024-04-12 11:05:30 -0300

    Add MeetupCache
    * Avoid consuming our quota from interactions from the bot

--- meetup.exs
+++ meetup.exs
@@ -91,3 +91,19 @@ defmodule Meetup do
     }
   end
 end
+
+defmodule MeetupCache do
+  use Agent
+
+  def start_link(_) do
+    Agent.start_link(fn -> [] end, name: __MODULE__)
+  end
+
+  def values do
+    Agent.get(__MODULE__, & &1)
+  end
+
+  def update(meetups) do
+    Agent.update(__MODULE__, fn(_state) -> meetups end)
+  end
+end
