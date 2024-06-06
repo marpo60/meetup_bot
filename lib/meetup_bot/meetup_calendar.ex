14f5fe1e170514891f9d0699869e2e0674e3b11d
Marcelo Dominguez <952074+marpo60@users.noreply.github.com>
2024-06-06 09:52:40 -0300

    Calendar support (#22)

---
+++ lib/meetup_bot/meetup_calendar.ex
@@ -0,0 +1,37 @@
+defmodule MeetupBot.MeetupCalendar do
+  def to_ics(meetups) do
+    """
+    BEGIN:VCALENDAR
+    VERSION:2.0
+    PRODID:MeetupBot
+    BEGIN:VTIMEZONE
+    TZID:America/Montevideo
+    X-LIC-LOCATION:America/Montevideo
+    BEGIN:STANDARD
+    TZOFFSETFROM:-0300
+    TZOFFSETTO:-0300
+    TZNAME:-03
+    DTSTART:19700101T000000
+    END:STANDARD
+    END:VTIMEZONE
+    #{for meetup <- meetups, do: build_event(meetup)}
+    END:VCALENDAR
+    """
+  end
+
+  defp build_event(meetup) do
+    """
+    BEGIN:VEVENT
+    DTSTART;TZID=America/Montevideo:#{format_datetime(meetup.datetime)}
+    DTEND;TZID=America/Montevideo:#{format_datetime(meetup.end_datetime)}
+    SUMMARY:#{meetup.title}
+    DESCRIPTION:#{meetup.event_url}
+    UID:#{meetup.id}@meetup_bot.local
+    END:VEVENT
+    """
+  end
+
+  defp format_datetime(datetime) do
+    Calendar.strftime(datetime, "%Y%m%dT%H%M%S")
+  end
+end
