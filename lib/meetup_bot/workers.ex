79228012e0d41496ed440d876e921bac37f2e430
Marcelo Dominguez <952074+marpo60@users.noreply.github.com>
2024-11-12 16:45:14 -0500

    Revert "Hardcode La Meetup 2024"
    This reverts commit aca2365f5ff6160fc8e23880f7b3743b92cf67e2.

--- lib/meetup_bot/workers.ex
+++ lib/meetup_bot/workers.ex
@@ -48,18 +48,6 @@ defmodule MeetupBot.MeetupCacheWorker do
         |> NaiveDateTime.add(30 * 24 * 60 * 60, :second)
 
       (Meetup.fetch_upcoming_meetups() ++ GDG.fetch_live_events())
-      |> Enum.filter(&(NaiveDateTime.to_date(&1.datetime) !== ~D[2024-10-19]))
-      |> Enum.concat([
-        %{
-          id: "la_meetup",
-          name: "LA MEETUP 2024",
-          title: "LA MEETUP 2024",
-          event_url: "https://www.owu.uy/la-meetup",
-          datetime: ~N[2024-10-19 09:00:00],
-          end_datetime: ~N[2024-10-19 18:00:00]
-        }
-      ])
-      |> Enum.filter(&(NaiveDateTime.compare(&1.datetime, NaiveDateTime.local_now()) == :gt))
       |> Enum.filter(&(NaiveDateTime.compare(&1.datetime, datetime_limit) == :lt))
       |> Enum.sort_by(& &1.datetime, NaiveDateTime)
       |> MeetupCache.update()
