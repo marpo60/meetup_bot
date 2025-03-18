63ed963b9f43fc4761a57b296ba8fc7683846e85
Marcelo Dominguez <952074+marpo60@users.noreply.github.com>
2025-03-18 14:34:29 -0300

    fix: manual worker test

--- config/test.exs
+++ config/test.exs
@@ -7,5 +7,6 @@ config :meetup_bot, MeetupBot.Repo,
 
 config :meetup_bot, :meetup, host: MeetupBot.Meetup.TestHost
 config :meetup_bot, :gdg, host: MeetupBot.GDG.TestHost
+config :meetup_bot, Oban, testing: :manual
 
 config :tower, :reporters, [Tower.EphemeralReporter]
